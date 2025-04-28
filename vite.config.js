1aadca90159ca325c4a8689e6da98bce511fcc7c
Philippe Barthelemy <philippebarthelemy@Philippes-MacBook-Pro.local>
2025-04-28 21:19:24 +0800

    Revert Vite external configuration to fix blank page issue

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/wvdi-ph/', // Set base path to match repository name for GitHub Pages
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa', 'react-icons/bs'],
-    }
-  }
 })
