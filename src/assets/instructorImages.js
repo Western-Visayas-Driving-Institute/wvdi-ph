f8450b9dc33a5f9c56b97a13899fa819042f75ce
Philippe Barthelemy <philippebarthelemy@Philippes-MacBook-Pro.local>
2025-04-28 23:56:22 +0800

    Remove Ian Mata Obrero from instructor team

--- src/assets/instructorImages.js
+++ src/assets/instructorImages.js
@@ -1,7 +1,6 @@
 // Import all instructor images directly
 import marcCelesJadia from './instructors/marc-celes-jadia.jpg';
 import joFranciscoRamos from './instructors/jo-francisco-ramos.jpg';
-import ianMataObrero from './instructors/ian-mata-obrero.jpg';
 import harryIntrepidoMorl from './instructors/harry-intrepido-morl.jpg';
 import ferdinandJilSulatJadia from './instructors/ferdinand-jil-sulat-jadia.jpg';
 import martinAmarGellecanao from './instructors/martin-amar-gellecanao.jpg';
@@ -12,7 +11,6 @@ import geneDeygabi from './instructors/gene-deygabi.jpg';
 const instructorImages = {
   'marc-celes-jadia': marcCelesJadia,
   'jo-francisco-ramos': joFranciscoRamos,
-  'ian-mata-obrero': ianMataObrero,
   'harry-intrepido-morl': harryIntrepidoMorl,
   'ferdinand-jil-sulat-jadia': ferdinandJilSulatJadia,
   'martin-amar-gellecanao': martinAmarGellecanao,
