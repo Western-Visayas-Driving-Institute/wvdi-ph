68f6ccab9802404e2d5b7b8f15cc9eb5e5bd16a0
philb75 <philb75@gmail.com>
2025-04-28 14:32:55 +0000

    Deploying to gh-pages from @ Western-Visayas-Driving-Institute/wvdi-ph@9858b7783c2440605aa1ca1a9dcc36b96887f798 🚀

--- src/components/services/Notice.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Notice = ({ text }) => (
-  <div className="my-6 px-4 py-3 rounded-md bg-[#f5f5f5] text-[#141f54] border-l-4 border-[#a00c0c] text-sm shadow-sm">
-    {text}
-  </div>
-);
-
-export default Notice;
